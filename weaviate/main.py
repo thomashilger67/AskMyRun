7a87f75907e6ef18303ff7f5b6eca2a3cff1e49a
thomashilger67 <hilger.thomas67@gmail.com>
2025-10-28 14:23:35 +0100

    add docker compose for weaviate

--- weaviate/main.py
+++ weaviate/main.py
@@ -1,19 +1,11 @@
 import weaviate
 import pandas as pd 
 import weaviate.classes as wvc
+from weaviate_client import wv_client
 
-client = weaviate.connect_to_custom(
-    http_host="34.155.19.255",
-    http_port=8080,
-    http_secure=False,
-    grpc_host="34.155.19.255",
-    grpc_port=50051,
-    grpc_secure=False,
-)
-
-collection = client.collections.use("strava_collection")
+collection = wv_client.collections.use("strava_collection")
 response = collection.aggregate.over_all(total_count=True)
 
 print(response.total_count)
 
-client.close()
+wv_client.close()
